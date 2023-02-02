b36f7ec253430d3a0b1d5f3630d8ea38db3a90fd
Jane Ro <janero95@gmail.com>
2023-02-02 15:17:43 +0900

    feat: pre-generate static props for homepage

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import fs from 'fs/promises';
+import Link from 'next/link.js';
+import path from 'path';
+
+function HomePage({ products }) {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  // use redirect prop to redirect page on condition
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
+  // use notFound prop to return 404 status on condition
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
